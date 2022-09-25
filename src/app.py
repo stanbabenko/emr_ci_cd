cf98be001363dbb4bc3ce206dc8986a7e3d47645
stanislav babenko <stanislavbabenko@his-hartogmbp19.eupepsia>
2022-09-25 10:04:31 -0400

    added dataframe

--- src/app.py
+++ src/app.py
@@ -9,11 +9,74 @@ from pyspark.sql import SQLContext
 import requests #todo: make shell scrip to install this
 from bs4 import BeautifulSoup
 import json
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+import os
+import findspark as fs
+
 
 def main():
     print("This line will be printed@@@.")
     run_aggregation()
     testFunction()
+    createDataFrame()
+
+def createDataFrame():
+    fs.init()
+    sc = SparkContext(appName="SDN-Aggregator")
+    spark = (SparkSession
+                     .builder
+                     .master("local[*]")
+                     .appName("Unit-tests")
+                     .getOrCreate())
+
+    input_schema = StructType([
+            StructField('StoreID', IntegerType(), True),
+            StructField('Location', StringType(), True),
+            StructField('Date', StringType(), True),
+            StructField('ItemCount', IntegerType(), True)
+        ])
+    input_data = [(1, "Bangalore", "2021-12-01", 5),
+                (2,"Bangalore" ,"2021-12-01",3),
+                (5,"Amsterdam", "2021-12-02", 10),
+                (6,"Amsterdam", "2021-12-01", 1),
+                (8,"Warsaw","2021-12-02", 15),
+                (7,"Warsaw","2021-12-01",99)]
+    input_df = spark.createDataFrame(data=input_data, schema=input_schema)
+    #2. Prepare an expected data frame which is the output that we expect.        
+    expected_schema = StructType([
+            StructField('Location', StringType(), True),
+            StructField('TotalItemCount', LongType(), True)
+            ])
+    
+    expected_data = [("Bangalore", 8),
+                    ("Warsaw", 114),
+                    ("Amsterdam", 11)]
+    expected_df = spark.createDataFrame(data=expected_data, schema=expected_schema)
+
+    #3. Apply our transformation to the input data frame
+    transformed_df = transform_data(input_df)
+
+    #4. Assert the output of the transformation to the expected data frame.
+    field_list = lambda fields: (fields.name, fields.dataType, fields.nullable)
+    fields1 = [*map(field_list, transformed_df.schema.fields)]
+    fields2 = [*map(field_list, expected_df.schema.fields)]
+    # Compare schema of transformed_df and expected_df
+    res = set(fields1) == set(fields2)
+
+    expected_df.show()
+    print(expected_df)
+    if expected_df is not None:
+        a = expected_df.collect()
+    
+    b = sorted(transformed_df.collect())
+    # assert
+    #assertTrue(res)
+    if (res):
+        print("res is true")
+    # Compare data in transformed_df and expected_df
+    c = sorted(expected_df.collect()), sorted(transformed_df.collect())
+    print(c)
 
 def testFunction():
     print("inside function 2")
